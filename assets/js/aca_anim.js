93af6a1d281549c35b711bbc58ac64d01642df80
BBoBBG <github@empirictechs.com>
2019-05-27 00:50:39 +0300

    Customer approved to publish

---
+++ assets/js/aca_anim.js
@@ -0,0 +1,8 @@
+var s = Snap(800, 800);
+s.text("50%", "50%", "check this shit").attr(
+    {
+        font: "300 100px Source Sans Pro",
+        textAnchor: "middle",
+        fill: "#fff"
+    }
+);
